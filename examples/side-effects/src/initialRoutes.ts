42a904d2f01a524675d77205b509a4cf706f14a7
Morris Allison III <morrisallison@users.noreply.github.com>
2022-12-31 00:08:27 -0800

    fix: Correct import name

--- examples/side-effects/src/initialRoutes.ts
+++ examples/side-effects/src/initialRoutes.ts
@@ -1,11 +1,11 @@
 import { createRouteEffects, createRoutes } from "@sables/framework/router";
 
-import { dogSearch } from "./sideEffects.js";
+import { searchDogs } from "./sideEffects.js";
 
 export const initialRoutes = createRoutes()
   .set("root", "/")
   .setEffects(() =>
     createRouteEffects().append("root", async (_action, effectAPI) => {
-      await dogSearch(effectAPI, { name: "Rex" });
+      await searchDogs(effectAPI, { name: "Rex" });
     })
   );
