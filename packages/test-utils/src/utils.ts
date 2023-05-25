a9890a61008342d8c89b0fcb5517466ace449dca
Morris Allison III <morrisallison@users.noreply.github.com>
2023-05-24 17:07:47 -0700

    style: Run prettier

--- packages/test-utils/src/utils.ts
+++ packages/test-utils/src/utils.ts
@@ -4,7 +4,7 @@ type LogKey = "info" | "log" | "warn" | "error";
 
 export function silenceLogs(vitest: VITest, key: LogKey) {
   const { afterAll, beforeAll, vi } = vitest;
-  let originalMethod: typeof console[LogKey];
+  let originalMethod: (typeof console)[LogKey];
 
   beforeAll(() => {
     originalMethod = console[key];
