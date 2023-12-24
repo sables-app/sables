c2fd876ba0b3f2ad47e2f982bc6a438cc33d2404
Morris Allison III <morrisallison@users.noreply.github.com>
2023-12-23 17:48:46 -0800

    feat: allow namespaces (#145)

--- packages/scripts/eslint.config.js
+++ packages/scripts/eslint.config.js
@@ -9,6 +9,10 @@ module.exports = {
   ],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
+    // Namespaces are be useful for grouping things within a module without exposing private functionality.
+    // They can also be used as a more readable alternative to a plain object.
+    // They should NOT be used as a replacement for a module.
+    "@typescript-eslint/no-namespace": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "react-hooks/exhaustive-deps": "error",
     // Not necessary when using JSX pragma
